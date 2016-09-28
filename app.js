906fb0cd68545937b3ef3573e2f32132c2fa8a9f
jdobrowner <j@jasondobrowner.com>
2016-09-28 07:56:09 -0700

    adding state

--- app.js
+++ app.js
@@ -6,9 +6,20 @@ function toggleHidden() {
 $('.plus-minus-button').click( function(event){
   var button = $(this);
   button.closest('div').children('.could-hide').toggleClass('hidden');
-
-  console.log(button.text());
   if (button.text() === '-') button.text('+');
   else button.text('-');
+  });
+}
+
+function beginQuiz() {
+$('.begin-quiz').click( function(event){
+  var button = $(this);
+  state.page = 1;
+  button.siblings('p').remove();
+  button.remove();
+  button.closest('.quiz').
+
+
+
   });
 }
--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 var state = {
-  
-}
+  // landing page = (0), question 1 = (1), solution 1 = (2) ...
+  // solution 10 = (20), results page = (21)
+  page: 0,
+  // 1 point for each correct answer
+  points: 0
+};
+
+var questionsList = [
+  ['Which is a logical equivalent to:'],
+  []
+
+];
+
+var choicesList = [
+  ['a && c','a && d','b && c','b && d'],
+  []
+
+];
+
+var answersList = [
+  'a && c',
+
+];
+
+var solutionText = [
+
+];
+
+var howGood = [
+  'godawful',
+  'pathetic',
+  'pathetic',
+  'lousy',
+  'lousy',
+  'unremarkable',
+  'unremarkable',
+  'competent',
+  'competent',
+  'superb',
+  'phenomenal'
+];
