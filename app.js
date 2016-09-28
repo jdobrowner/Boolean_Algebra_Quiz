fb18fb4f1ed52b1bb3efa00da8aea24fa9a75a2e
jdobrowner <j@jasondobrowner.com>
2016-09-28 12:39:55 -0700

    improved structure of toggles

--- app.js
+++ app.js
@@ -1,25 +1,102 @@
-$( function() {
-  toggleHidden();
+//*-------------- call functions when document is loaded -----------------*
+
+$(function() {
+    setUpQuizToolToggleButtons();
 });
 
-function toggleHidden() {
-$('.plus-minus-button').click( function(event){
-  var button = $(this);
-  button.closest('div').children('.could-hide').toggleClass('hidden');
-  if (button.text() === '-') button.text('+');
-  else button.text('-');
-  });
+//*--------- set up scratch paper and logic table toggle -------------*
+
+function setUpQuizToolToggleButtons() {
+
+    $('#paper-header').click( function(event) {
+        toggleQuizTool($(this), quizApp.state.isPaperShowing);
+        quizApp.state.isPaperShowing = !quizApp.state.isPaperShowing;
+    });
+
+    $('#table-header').click( function(event) {
+        toggleQuizTool($(this), quizApp.state.isTableShowing);
+        quizApp.state.isTableShowing = !quizApp.state.isTableShowing;
+    });
 }
 
-function beginQuiz() {
+function toggleQuizTool($button, isShowing) {
+    if (isShowing) {
+        $button.next().addClass('hidden');
+        $button.find('span').text('+');
+    }
+    else {
+        $button.next().removeClass('hidden');
+        $button.find('span').text('-');
+    }
+}
+
+
+//*------------------ enter first quiz question ---------------------------*
+
+/*function beginQuiz() {
 $('.begin-quiz').click( function(event){
-  var button = $(this);
   state.page = 1;
-  button.siblings('p').remove();
-  button.remove();
-  button.closest('.quiz').
-
+  $('#quiz').children().remove().
 
 
   });
-}
+}*/
+
+
+//*------- hold the state and content of the app in global quizApp ---------*
+
+var quizApp = {
+
+    //*------------- state of the landing page ---------------*
+
+    state : {
+        // landing page = (0), question 1 = (1), solution 1 = (2) ...
+        // solution 10 = (20), results page = (21)
+        page: 0,
+        // 1 point for each correct answer
+        points: 0,
+
+        isPaperShowing : true,
+        isTableShowing : true
+
+    },
+
+
+    //*------------ app content ----------------------------*
+
+    questionsList : [
+        ['Which is a logical equivalent to:'],
+        []
+
+    ],
+
+    choicesList : [
+        ['a && c', 'a && d', 'b && c', 'b && d'],
+        []
+
+    ],
+
+    answersList : [
+        'a && c',
+
+    ],
+
+    solutionText : [
+
+    ],
+
+    howGood : [
+        'godawful',
+        'pathetic',
+        'pathetic',
+        'lousy',
+        'lousy',
+        'unremarkable',
+        'unremarkable',
+        'competent',
+        'competent',
+        'superb',
+        'phenomenal'
+    ]
+
+};
